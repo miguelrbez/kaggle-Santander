c7c0b69101282bd9bd7c9fb2827a710133b56db3
Miguel Rodriguez <miguelrbez@gmail.com>
2019-07-17 03:16:57 -0500

    Worked on santander-classification.py Implemented: - Default Random forest classifier - Added train scoring in precision, recall and F1 score function Notes: - Default SGD classifier has high bias - Default Random forest classifier has high variance

--- santander-classification.py
+++ santander-classification.py
@@ -70,47 +70,95 @@ X_train_scaled_red, X_test_scaled_red = scale_data(X_train_red, X_test_red)
 
 # Set classifiers
 from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 sgd_clf = SGDClassifier(random_state=8)
+forest_clf = RandomForestClassifier(random_state=8)
 
 
-# Print cross-validate precision, recall and F1 score for SGD classifier
+# Print cross-validate precision, recall and F1 score for classifier
 from sklearn.model_selection import cross_validate
 
 def clf_scores(clf, X=X_train_scaled, y=y_train, title=None):
     scoring = ["precision", "recall", "f1"]
     scores = cross_validate(clf, X, y,
                             cv=3, scoring=scoring)
+    train_precision = np.mean(scores["train_precision"])
+    train_recall = np.mean(scores["train_recall"])
+    train_f1 = np.mean(scores["train_f1"])
     test_precision = np.mean(scores["test_precision"])
     test_recall = np.mean(scores["test_recall"])
     test_f1 = np.mean(scores["test_f1"])
     if title:
         print(title)
-    print(f"Precision = {test_precision:.4f}")
-    print(f"Recall = {test_recall:.4f}")
-    print(f"F1 score = {test_f1:.4f}")
+    print(f"Train Precision = {train_precision:.4f}")
+    print(f"Test Precision = {test_precision:.4f}")
+    print(f"Train Recall = {train_recall:.4f}")
+    print(f"Test Recall = {test_recall:.4f}")
+    print(f"Train F1 score = {train_f1:.4f}")
+    print(f"Test F1 score = {test_f1:.4f}")
     return scores
 
+
 # Scores for SGD classifier using reduced and not-reduced X_train
-sgd_clf_scores = clf_scores(sgd_clf, X_train_scaled_red, y_train_red, title="SGD (reduced)")
+
+# sgd_clf_scores = clf_scores(sgd_clf, X_train_scaled_red, y_train_red, title="SGD (reduced)")
 # # SGD (reduced)
-# # Precision = 0.3869
-# # Recall = 0.3364
-# # F1 score = 0.3594
+# # Train Precision = 0.3882
+# # Test Precision = 0.3869
+# # Train Recall = 0.3332
+# # Test Recall = 0.3364
+# # Train F1 score = 0.3584
+# # Test F1 score = 0.3594
+# # High bias
+#
 # sgd_clf_scores = clf_scores(sgd_clf, title="SGD (not-reduced)")
 # # SGD (not-reduced)
-# # Precision = 0.4057
-# # Recall = 0.2960
-# # F1 score = 0.3403
+# # Train Precision = 0.4187
+# # Test Precision = 0.4057
+# # Train Recall = 0.3034
+# # Test Recall = 0.2960
+# # Train F1 score = 0.3500
+# # Test F1 score = 0.3403
+# # High bias
 
 
-# Compute scores for classifier
+# Scores for random forest classifier using reduced and not-reduced X_train
+
+# forest_clf_scores = clf_scores(forest_clf, X_train_scaled_red, y_train_red, title="Forest (reduced)")
+# # Forest (reduced)
+# # Train Precision = 0.9999
+# # Test Precision = 0.5301
+# # Train Recall = 0.8446
+# # Test Recall = 0.0129
+# # Train F1 score = 0.9157
+# # Test F1 score = 0.0253
+# # High variance
+
+# forest_clf_scores = clf_scores(forest_clf, title="Forest (not-reduced)")
+# # Forest (not-reduced)
+# # Train Precision = 1.0000
+# # Test Precision = 0.5542
+# # Train Recall = 0.8506
+# # Test Recall = 0.0156
+# # Train F1 score = 0.9192
+# # Test F1 score = 0.0304
+# # High variance
+
+
+# Compute y scores (y_proba) for classifiers
 from sklearn.model_selection import cross_val_predict
 
 def predict_scores_sgd(X=X_train_scaled, y_true=y_train):
     y_scores_sgd = cross_val_predict(sgd_clf, X, y_true,
                                      cv=3, method='decision_function')
     return y_true, y_scores_sgd
+
+def predict_scores_forest(X=X_train_scaled, y_true=y_train):
+    y_scores_forest = cross_val_predict(forest_clf, X, y_true,
+                                        cv=3, method='predict_proba')[:, 1]
+    return y_true, y_scores_forest
+
 
 # Plot precision-recall curve function
 from sklearn.metrics import precision_recall_curve
@@ -129,9 +177,9 @@ def plot_precision_recall_curve(y_true, y_scores, title=None):
     plt.show()
 
 
-# Predict scores and plot precision-recall curve for SGD classifier
-def SGD_precision_recall_curves():
-    y_true, y_scores_sgd = predict_scores_sgd(X_train_scaled, y_train)
-    plot_precision_recall_curve(y_true, y_scores_sgd, "SGD")
+# Predict y_scores and plot precision-recall curve for classifiers
+y_true, y_scores_sgd = predict_scores_sgd(X_train_scaled, y_train)
+plot_precision_recall_curve(y_true, y_scores_sgd, "SGD")
 
-SGD_precision_recall_curves()
+y_true, y_scores_forest = predict_scores_forest(X_train_scaled, y_train)
+plot_precision_recall_curve(y_true, y_scores_forest, "Forest")
